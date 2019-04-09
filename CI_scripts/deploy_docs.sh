1bd9d98b11567d4d3714ee86ec432482d23c51bc
Yuichi Motoyama <yomichi@tsg.jp>
2019-04-09 23:28:07 +0900

    omit 'set -e'

--- CI_scripts/deploy_docs.sh
+++ CI_scripts/deploy_docs.sh
@@ -1,5 +1,3 @@
-set -e
-
 # This is a pull request, finish.
 if [ "_$TRAVIS_PULL_REQUEST" != "_false" ] ;then travis_terminate 0; fi
 # This is neither master nor tag, finish.
@@ -18,10 +16,8 @@ cp -r build/doc/jp/html/* ${DOCDIR}/jp
 cp -r build/doc/en/html/* ${DOCDIR}/en
 # cp -r build/doc/en/pdf/test_travis.pdf ${DOCDIR}/en
 git add ${DOCDIR}
-set +e
 git commit -m "Update by TravisCI"
 ST=$?
-set -e
 if [ $ST == 0 ]; then
   git push "https://${GH_TOKEN}@github.com/yomichi/test-travis.git" gh-pages:gh-pages --follow-tags > /dev/null 2>&1
 fi
