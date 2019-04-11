7ac39b60d93120c97d1dbf8d3e4c5326dedb9fea
Yuichi Motoyama <yomichi@tsg.jp>
2019-04-11 14:08:53 +0900

    fixed maketarball

--- CI_scripts/make_tarball.sh
+++ CI_scripts/make_tarball.sh
@@ -2,7 +2,7 @@
 if [ -z "$TRAVIS_TAG" ]; then exit 0; fi
 
 DIRNAME=test-travis-${TRAVIS_TAG}
-wget https://github.com/${TRAVIS_REPO_SLUG} -O tarball.tar.gz
+wget https://github.com/${TRAVIS_REPO_SLUG}/archive/${TRAVIS_TAG}.tar.gz -O tarball.tar.gz
 mkdir $DIRNAME
 tar -xz -C $DIRNAME --strip-components=1 -f tarball.tar.gz
 cp -r build/doc/jp/pdf/test_travis.pdf ${DIRNAME}/test_travis-jp-${TRAVIS_TAG}.pdf
