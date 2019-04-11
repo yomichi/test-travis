56b963c883de24cbf3582532620edb3f67759529
Yuichi Motoyama <yomichi@tsg.jp>
2019-04-11 12:03:44 +0900

    use sshkey instead of api token

--- CI_scripts/deploy_docs.sh
+++ CI_scripts/deploy_docs.sh
@@ -3,8 +3,15 @@ if [ "_$TRAVIS_PULL_REQUEST" != "_false" ] ;then exit 0; fi
 # This is neither master nor tag, finish.
 if [ "_$TRAVIS_BRANCH" != "_master" ] && [ -z "$TRAVIS_TAG" ] ; then exit 0; fi
 
+openssl aes-256-cbc -K $encrypted_12106f7bbd8a_key -iv $encrypted_12106f7bbd8a_iv -in ssh_key.enc -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+git config --global user.email "yomichi@tsg.jp"
+git config --global user.name "Yuichi Motoyama"
+
+git remote set-url origin git@github.com:$(git config --local remote.origin.url | cut -d/ -f4-5)
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-git fetch # --unshallow
+git fetch --unshallow origin
 git checkout gh-pages
 DOCDIR=docs/${TRAVIS_BRANCH}
 mkdir -p ${DOCDIR}
@@ -19,6 +26,6 @@ git add ${DOCDIR}
 git commit -m "Update by TravisCI"
 ST=$?
 if [ $ST == 0 ]; then
-  git push "https://${GH_TOKEN}@github.com/yomichi/test-travis.git" gh-pages:gh-pages --follow-tags > /dev/null 2>&1
+  git push origin gh-pages:gh-pages --follow-tags > /dev/null 2>&1
 fi
 
